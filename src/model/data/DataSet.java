347f2038bf8746d9851482f1de8a4cb010f09e13
stefa17pj <stelsant@espol.edu.ec>
2021-01-18 18:01:42 -0500

    Cambios

--- src/model/data/DataSet.java
+++ src/model/data/DataSet.java
@@ -9,8 +9,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import model.umbral.*;
 
 /**
@@ -18,6 +21,7 @@ import model.umbral.*;
  * @author Willy Mateo
  */
 public class DataSet {
+
     private Map<String, ArrayList<Boolean>> dataset;
     private Map<String, Double> gini;
 
@@ -25,16 +29,16 @@ public class DataSet {
         dataset = new HashMap();
         gini = new HashMap(12);
     }
-    
-    public void loadData(String filePath){
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+    public void loadData(String filePath) {
+        try ( BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line = br.readLine();
             String[] atributes = line.strip().split(",");
             for (String atributo : atributes) {
                 dataset.put(atributo, new ArrayList<>(299));
                 gini.put(atributo, 0.0);
             }
-            while ((line = br.readLine()) != null) {                
+            while ((line = br.readLine()) != null) {
                 String[] parts = line.strip().split(",");
                 valueConverter(parts[0], atributes[0], new MinAreTrueAdjuster(62));
                 valueConverter(parts[1], atributes[1], new DefaultAdjuster());
@@ -50,23 +54,23 @@ public class DataSet {
                 valueConverter(parts[11], atributes[11], new DefaultAdjuster());
             }
         } catch (IOException ioE) {
-            System.out.println("Error al tratar de abrir el archivo:\n" +ioE);
+            System.out.println("Error al tratar de abrir el archivo:\n" + ioE);
         }
     }
-    
-    public void actGini(String target){
+
+    public void actGini(String target) {
         for (String key : gini.keySet()) {
             gini.put(key, calculateGini(key, target));
         }
     }
-    
-    private void valueConverter(String stringValue, String key, Adjuster adjuster){
+
+    private void valueConverter(String stringValue, String key, Adjuster adjuster) {
         double value = Double.parseDouble(stringValue);
         boolean converted = UmbralHandler.convert(adjuster, value);
         dataset.get(key).add(converted);
     }
 
-    private double calculateGini(String attribute, String target){
+    private double calculateGini(String attribute, String target) {
         ArrayList<Boolean> primerAtributo = dataset.get(attribute);
         ArrayList<Boolean> segundoAtributo = dataset.get(target);
         int contadorSiPrimeroSiSegundo = 0;
@@ -76,41 +80,69 @@ public class DataSet {
         int contadorSi = 0;
         int contadorNo = 0;
         double giniValue;
-        
+
         /* En caso que el atributo y el target sean el mismo*/
-        if(attribute.equals(target)){
-            for(int i=0; i<primerAtributo.size(); i++){
-                if(primerAtributo.get(i)==true){
+        if (attribute.equals(target)) {
+            for (int i = 0; i < primerAtributo.size(); i++) {
+                if (primerAtributo.get(i) == true) {
                     contadorSi++;
-                } else{
+                } else {
                     contadorNo++;
                 }
             }
-            giniValue = 1 - Math.pow(((double)contadorSi/primerAtributo.size()),2) - Math.pow(((double)contadorNo/primerAtributo.size()),2);
+            giniValue = 1 - Math.pow(((double) contadorSi / primerAtributo.size()), 2) - Math.pow(((double) contadorNo / primerAtributo.size()), 2);
         } else {
             /*El atributo y el target son diferentes*/
-            for(int i=0; i<primerAtributo.size(); i++){
-                if(primerAtributo.get(i)==true && segundoAtributo.get(i)==true){
+            for (int i = 0; i < primerAtributo.size(); i++) {
+                if (primerAtributo.get(i) == true && segundoAtributo.get(i) == true) {
                     contadorSiPrimeroSiSegundo++;
                     contadorSi++;
-                } else if(primerAtributo.get(i)==true && segundoAtributo.get(i)==false){
+                } else if (primerAtributo.get(i) == true && segundoAtributo.get(i) == false) {
                     contadorSiPrimeroNoSegundo++;
                     contadorSi++;
-                } else if(primerAtributo.get(i)==false && segundoAtributo.get(i)==true){
+                } else if (primerAtributo.get(i) == false && segundoAtributo.get(i) == true) {
                     contadorNoPrimeroSiSegundo++;
                     contadorNo++;
-                }else{
+                } else {
                     contadorNoPrimeroNoSegundo++;
                     contadorNo++;
                 }
             }
-            double giniSiPrimero = 1 - Math.pow(((double)contadorSiPrimeroSiSegundo/contadorSi),2) - Math.pow(((double)contadorSiPrimeroNoSegundo/contadorSi),2);
-            double giniNoPrimero = 1 - Math.pow(((double)contadorNoPrimeroSiSegundo/contadorNo),2) - Math.pow(((double)contadorNoPrimeroNoSegundo/contadorNo),2);
-            giniValue = ((double)contadorSi/primerAtributo.size())*giniSiPrimero + ((double)contadorNo/primerAtributo.size())*giniNoPrimero;
+            double giniSiPrimero = 1 - Math.pow(((double) contadorSiPrimeroSiSegundo / contadorSi), 2) - Math.pow(((double) contadorSiPrimeroNoSegundo / contadorSi), 2);
+            double giniNoPrimero = 1 - Math.pow(((double) contadorNoPrimeroSiSegundo / contadorNo), 2) - Math.pow(((double) contadorNoPrimeroNoSegundo / contadorNo), 2);
+            giniValue = ((double) contadorSi / primerAtributo.size()) * giniSiPrimero + ((double) contadorNo / primerAtributo.size()) * giniNoPrimero;
         }
         return giniValue;
     }
-    
+
+    public ArrayList<DataSet> segmentarData(String atributo) {
+        ArrayList<DataSet> data = new ArrayList<>();
+        DataSet datosP = new DataSet();
+        DataSet datosN = new DataSet();
+        String atributeMin = null;
+
+        DataSet dataset = new DataSet();
+        dataset.loadData("resources/pacientes.csv");
+        dataset.actGini(atributo);
+        double min = Collections.min(dataset.gini.values());
+        //- Obtiene atributo con menor gini
+        for (Map.Entry<String, Double> entry : dataset.getGini().entrySet()) {
+            if (entry.getValue() == min) {
+                atributeMin = entry.getKey();
+            }
+        }
+        //- Crea datosP y datosN
+        //- Retorna datosP y datosN (en una estructura: lista o arreglo). 
+        if (atributeMin.equals(atributo)) {
+            return null;
+        } else {
+            //La parte de segmentar
+            data.add(datosP);
+            data.add(datosN);
+        }
+        return data;
+    }
+
     public Map<String, ArrayList<Boolean>> getDataset() {
         return dataset;
     }
@@ -118,5 +150,5 @@ public class DataSet {
     public Map<String, Double> getGini() {
         return gini;
     }
-    
+
 }
